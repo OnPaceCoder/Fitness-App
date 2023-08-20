0e5e6be8715c43c3a12fd96b0d470e845fd379ab
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-08-20 21:48:56 +0530

    Add: Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material'
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto" >
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
